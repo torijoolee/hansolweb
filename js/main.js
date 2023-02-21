8aab0f974fce661c2b5138bcc975614b0c009f62
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-02-21 23:02:01 +0900

    JS: change headerimage

--- js/main.js
+++ js/main.js
@@ -2,11 +2,38 @@
   // header carusel
   const prevBtn = document.querySelector(".prev");
   const nextBtn = document.querySelector(".next");
-  const visualElems = document.querySelectorAll(".img-con .slide");
+  const visualElem = document.querySelector(".img-con .slide");
   const headerBg = ["main01.jpg", "main02.jpg", "main03.jpg", "main04.jpg"];
+  const barElems = document.querySelectorAll(".control li a");
 
-  function slideImage() {
-    visualElems.style.background = "`url${}`";
+  let currentPage = 0;
+  let totalPage = headerBg.length;
+
+  prevBtn.addEventListener("click", function () {
+    if (currentPage > 0) {
+      currentPage--;
+    } else {
+      currentPage = totalPage - 1;
+    }
+    console.log(currentPage);
+    changeImage();
+  });
+
+  nextBtn.addEventListener("click", function () {
+    if (currentPage < totalPage - 1) {
+      currentPage++;
+    } else {
+      currentPage = 0;
+    }
+    console.log(currentPage);
+    changeImage();
+  });
+
+  function changeImage() {
+    visualElem.style.background = `url(/image/${headerBg[currentPage]}) no-repeat 0 0 `;
+    barActive();
   }
-  slideImage();
+  changeImage();
+
+  function barActive() {}
 })();
