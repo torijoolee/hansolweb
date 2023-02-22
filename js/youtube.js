29e5c8c1c29198d5a1e2e13f216696fd9db9814b
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-02-22 22:52:47 +0900

    JS-youtubebar

--- js/youtube.js
+++ js/youtube.js
@@ -2,29 +2,30 @@
   const youtubeBar = document.querySelectorAll(".youtube .bar-lg");
   const container = document.querySelector(".youtube-container");
 
-  let current = 0;
-
   for (let i = 0; i < youtubeBar.length; i++) {
     youtubeBar[i].dataset.link = i;
-  }
 
-  youtubeBar.forEach((bar) => {
-    bar.addEventListener("click", (e) => {
-      // console.log(e.target.dataset.link);
-      current = parseInt(e.target.dataset.link);
-      console.log(current);
-      if (current == 0) {
-        container.style.transform = `translateX(0vw)`;
-        container.style.transition = `0.5s`;
-      }
-      if (current == 1) {
-        container.style.transform = `translateX(-80vw)`;
-        container.style.transition = `0.5s`;
-      }
-      if (current == 2) {
-        container.style.transform = `translateX(-160vw)`;
-        container.style.transition = `0.5s`;
-      }
+    youtubeBar.forEach((bar) => {
+      bar.addEventListener("click", (e) => {
+        youtubeBar[i].classList.remove("active");
+        // console.log(e.target.dataset.link);
+        current = parseInt(e.target.dataset.link);
+        console.log(current);
+        youtubeBar[current].classList.add("active");
+
+        if (current == 0) {
+          container.style.transform = `translateX(0vw)`;
+          container.style.transition = `0.5s`;
+        }
+        if (current == 1) {
+          container.style.transform = `translateX(-80vw)`;
+          container.style.transition = `0.5s`;
+        }
+        if (current == 2) {
+          container.style.transform = `translateX(-160vw)`;
+          container.style.transition = `0.5s`;
+        }
+      });
     });
-  });
+  }
 })();
