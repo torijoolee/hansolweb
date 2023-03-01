5396116a07b2e869e7d75627fa77503a316ff168
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-01 16:09:36 +0900

    JS: blog

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,20 @@
 (() => {
   const blogBar = document.querySelectorAll(".blog .bar-lg");
-  const container = document.querySelector(".scroll-con");
+  const container = document.querySelector(".blog-contents");
+  const blogItem = document.querySelector(".blog-item");
+
+  const itemWidth = blogItem.offsetWidth;
 
   let current = 0;
 
+  // viewport
+  const blogViewport = document.querySelector(".viewport");
+  blogViewport.style.background = "transparent";
+  blogViewport.style.width = `${itemWidth * 4}px`;
+  blogViewport.style.height = "500px";
+  blogViewport.style.transform = `translateX(${itemWidth / 2}px)`;
+
+  // moving
   for (let i = 0; i < blogBar.length; i++) {
     blogBar[i].dataset.link = i;
 
@@ -14,15 +25,15 @@
         current = parseInt(e.target.dataset.link);
         blogBar[current].classList.add("active");
         if (current == 0) {
-          container.style.transform = `translateX(0vw)`;
+          container.style.transform = `translateX(0)`;
           container.style.transition = `0.5s`;
         }
         if (current == 1) {
-          container.style.transform = `translateX(-80vw)`;
+          container.style.transform = `translateX(${-itemWidth * 4}px)`;
           container.style.transition = `0.5s`;
         }
         if (current == 2) {
-          container.style.transform = `translateX(-161vw)`;
+          container.style.transform = `translateX(${-itemWidth * 8}px)`;
           container.style.transition = `0.5s`;
         }
       });
