2750af18705ef97f296cae633215e6f343511f89
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-01 20:34:47 +0900

    JS: blog media

--- js/blog.js
+++ js/blog.js
@@ -1,42 +1,56 @@
 (() => {
-  const blogBar = document.querySelectorAll(".blog .bar-lg");
-  const container = document.querySelector(".blog-contents");
-  const blogItem = document.querySelector(".blog-item");
+  if (matchMedia("screen and (min-width: 640px)").matches) {
+    console.log("desktop");
+    const blogBar = document.querySelectorAll(".blog .bar-lg");
+    const container = document.querySelector(".blog-contents");
+    const blogItem = document.querySelector(".blog-item");
+    const itemWidth = blogItem.offsetWidth;
 
-  const itemWidth = blogItem.offsetWidth;
+    let current = 0;
 
-  let current = 0;
+    // viewport
+    const blogViewport = document.querySelector(".viewport");
+    blogViewport.style.background = "transparent";
+    blogViewport.style.width = `${itemWidth * 4}px`;
+    blogViewport.style.height = "500px";
+    blogViewport.style.transform = `translateX(${itemWidth / 2}px)`;
 
-  // viewport
-  const blogViewport = document.querySelector(".viewport");
-  blogViewport.style.background = "transparent";
-  blogViewport.style.width = `${itemWidth * 4}px`;
-  blogViewport.style.height = "500px";
-  blogViewport.style.transform = `translateX(${itemWidth / 2}px)`;
+    // moving
+    for (let i = 0; i < blogBar.length; i++) {
+      blogBar[i].dataset.link = i;
 
-  // moving
-  for (let i = 0; i < blogBar.length; i++) {
-    blogBar[i].dataset.link = i;
-
-    blogBar.forEach((bar) => {
-      bar.addEventListener("click", (e) => {
-        blogBar[i].classList.remove("active");
-        // console.log(e.target.dataset.link);
-        current = parseInt(e.target.dataset.link);
-        blogBar[current].classList.add("active");
-        if (current == 0) {
-          container.style.transform = `translateX(0)`;
-          container.style.transition = `0.5s`;
-        }
-        if (current == 1) {
-          container.style.transform = `translateX(${-itemWidth * 4}px)`;
-          container.style.transition = `0.5s`;
-        }
-        if (current == 2) {
-          container.style.transform = `translateX(${-itemWidth * 8}px)`;
-          container.style.transition = `0.5s`;
-        }
+      blogBar.forEach((bar) => {
+        bar.addEventListener("click", (e) => {
+          blogBar[i].classList.remove("active");
+          // console.log(e.target.dataset.link);
+          current = parseInt(e.target.dataset.link);
+          blogBar[current].classList.add("active");
+          if (current == 0) {
+            container.style.transform = `translateX(0)`;
+            container.style.transition = `0.5s`;
+          }
+          if (current == 1) {
+            container.style.transform = `translateX(${-itemWidth * 4}px)`;
+            container.style.transition = `0.5s`;
+          }
+          if (current == 2) {
+            container.style.transform = `translateX(${-itemWidth * 8}px)`;
+            container.style.transition = `0.5s`;
+          }
+        });
       });
-    });
+    }
+  } else {
+    console.log("mobile");
+    const blogSmallView = document.querySelector(".viewport");
+    const smallBlogWidth = document.querySelector(".blog").offsetWidth;
+    console.log(`width: ${smallBlogWidth}`);
+
+    blogSmallView.style.height = "500px";
+    blogSmallView.style.transform = `translateX(32px)`;
+    blogSmallView.style.width = `${640 - 64}px`;
   }
+  window.onresize = function () {
+    document.location.reload();
+  };
 })();
